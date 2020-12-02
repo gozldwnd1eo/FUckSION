90b96806d5efeaf2f3bda707bdc3859280464057
liche <lichee55@gmail.com>
2020-12-02 21:14:37 +0900

    규철이가 왜 8비트를 미는지 몰랐다

--- MovieSysClient/CustomerMainController.java
+++ MovieSysClient/CustomerMainController.java
@@ -102,7 +102,11 @@ public class CustomerMainController implements Initializable {
                 Myconn.is.read(buf);
                 int packetType = buf[0];
                 int packetCode = buf[1];
-                int packetBodyLen = buf[3];
+                byte[] b=new byte[2];
+                System.arraycopy(buf, 3, b, 0, 2);
+                int packetBodyLen=(b[0]&0x000000ff<<8);
+                packetBodyLen+=(b[1]&0x000000ff);
+                // int packetBodyLen = buf[3];
                 int packetFlag = buf[5];
                 last = buf[6];
                 int packetSeqNum = buf[7];
